949117985775ccb71770e58d0575aacb3b7fccca
Sergei-rgb@ <sergei25102007@bk.ru>
2024-09-24 16:57:39 +0400

    main

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
 var migrating = true;
 
+var fly = function(num) { 
+    var sound = "Flying!";
+    function wingFlapper() {
+        console.log(sound);
+}
+    for (var i = 0; i < num; i++) {
+        wingFlapper();
+}
+};
+
 function quack(num) {
+var sound = "Quack";
+var quacker = function() {
+    console.log(sound);
+};
     for (i = 0; i < num; i++) {
-        console.log("Quack!");
+        quacker();
     }
 }
-
-var fly = function(num) {
-    for (i = 0; i < num; i++) {
-        console.log("Flying!");
-    }
-};
 
 if (migrating) {
     quack(4);
     fly(4);
 }
+
+var justAvar = "Oh, don't you worry about it, I'm GLOBAL";
+
+function whereAreYou() {
+    var justAvar = "Just an every day LOCAL";
+    
+    function inner() {
+        return justAvar;
+    }
+
+    return inner;
+}
+
+var innerT = "TEST"
+function innerFunction() {
+    var innerT = "Just an every day UPDATE";
+    
+    function inner() {
+        return innerT;
+    }
+
+    return inner;
+}
+
+var result = whereAreYou();
+var result = innerFunction();
+console.log(result);
